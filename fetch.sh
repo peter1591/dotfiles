cd8862cd81405be4dbbcdd1f55efc9f2d8d6012c
Shen-En Shih <shshih@tesla.com>
2022-04-27 22:35:21 -0700

    make script compatible on mac
    but user need to install homebrew and git first though

--- fetch.sh
+++ fetch.sh
@@ -1,18 +1,17 @@
-# TODO(Darkpi): Move these checks (except git) to install script?
-if ! command -v apt-get >/dev/null 2>&1; then
-  echo $'\e[1;31mNo apt-get?!\e[m'
-  exit 1
+if command -v apt-get >/dev/null 2>&1; then
+	if ! sudo -l >/dev/null 2>&1; then
+		echo $'\e[1;31mIt appears that you can\'t sudo, but the script assumes sudo privilege in lots of place...\e[m'
+		exit 1
+	fi
+	if ! command -v git >/dev/null 2>&1; then
+		echo $'\e[1;33mgit not installed, installing...\e[m'
+		sudo apt-get update
+		sudo apt-get -y install git
+	fi
 fi
-# XXX(Darkpi): Do we really want this?
-if ! sudo -l >/dev/null 2>&1; then
-  echo $'\e[1;31mIt appears that you can\'t sudo, but the script assumes sudo privilege in lots of place...\e[m'
-  exit 1
-fi
-if ! command -v git >/dev/null 2>&1; then
-  echo $'\e[1;33mgit not installed, installing...\e[m'
-  sudo apt-get update
-  sudo apt-get -y install git
-fi
+
 git clone https://github.com/peter1591/dotfiles ~/dotfiles
+
 cd ~/dotfiles
+
 ./install
